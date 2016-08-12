5d15de57980be162fa314357dd759ef102e6e6f1
Brent Baccala <cosine@freesoft.org>
2016-08-12 05:29:10 -1000

    add basic TCP server loop

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 
 netmsg: netmsg.cc
-	g++ -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -I. -o netmsg netmsg.cc
+	g++ -std=c++11 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -I. -o netmsg netmsg.cc -lpthread
--- netmsg.cc
+++ netmsg.cc
@@ -25,6 +25,12 @@
 #include <argp.h>
 #include <version.h>
 
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <thread>
+
 extern "C" {
 #include <hurd.h>
 #include <hurd/fsys.h>
@@ -69,6 +75,122 @@ parse_opt (int key, char *arg, struct argp_state *state)
 }
 
 static struct argp argp = { options, parse_opt, args_doc, doc };
+
+#define TM_BUF_SIZE 1024
+
+void
+tcpServerHandler(int inSocket)
+{
+  int errorCode;
+  char testBuffer[TM_BUF_SIZE];
+
+  while (1)
+    {
+
+      /* Receive data on the new socket created by accept */
+      errorCode = recv(inSocket, testBuffer, TM_BUF_SIZE, 0);
+
+      /* Make sure there wasn’t an error */
+      if (errorCode < 0)
+	{
+	  close(inSocket);
+	  break;
+	}
+
+      /* Receiving 0 bytes of data means the connection has been
+       * closed.  If this happens, close the new socket and break out
+       * of this (the inner) loop.
+       */
+
+      if (errorCode == 0)
+	{
+	  close(inSocket);
+	  break;
+	}
+    }
+}
+
+void
+tcpServer(int listenPort)
+{
+  int listenSocket;
+  int newSocket;
+  struct sockaddr_in sourceAddr;
+  struct sockaddr_in destAddr;
+  socklen_t addrLen;
+  int errorCode;
+
+  /* Specify the address family */
+  destAddr.sin_family = AF_INET;
+
+  /* Specify the dest port, the one we'll bind to */
+  destAddr.sin_port = htons(listenPort);
+
+  /* Specify the destination IP address (our IP address). Setting
+   * this value to 0 tells the stack that we don’t care what IP
+   * address we use - it should listen on all of them.
+   */
+  destAddr.sin_addr.s_addr = inet_addr("0.0.0.0");
+
+  /* Create a socket */
+  listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+  /* Make sure the socket was created successfully */
+  if (listenSocket < 0)
+    {
+      error (2, listenSocket, "TCP socket");
+    }
+
+  /*
+   * Bind the socket to the port and address at which we wish to
+   * receive data
+   */
+  errorCode = bind(listenSocket, (const struct sockaddr *) &destAddr, sizeof(destAddr));
+
+  /* Check for an error in bind */
+  if (errorCode < 0)
+    {
+      error (2, listenSocket, "TCP bind");
+    }
+
+  /* Set up the socket as a listening socket */
+  errorCode = listen(listenSocket, 10);
+
+  /* Check for an error in listen */
+  if (errorCode < 0)
+    {
+      error (2, errorCode, "TCP listen");
+    }
+
+  /* Do this forever... */
+  while (1)
+    {
+
+      /* Get the size of the sockaddr_in structure */
+      addrLen = sizeof(sourceAddr);
+
+      /* Accept an incoming connection request. The address/port info for
+       * the connection’s source is stored in sourceAddr. The length of
+       * the data written to sourceAddr is stored in addrLen. The
+       * initial value of addrLen is checked to make sure too many
+       * bytes are not written to sourceAddr.
+       */
+
+      newSocket = accept(listenSocket, (struct sockaddr *) &sourceAddr, &addrLen);
+
+      /* Check for an error in accept */
+
+      if (newSocket < 0)
+	{
+	  error (2, newSocket, "TCP accept");
+	}
+      else
+	{
+	  /* Spawn a new thread to handle the new socket */
+	  new std::thread(tcpServerHandler, newSocket);
+	}
+    }
+}
 
 
 int
