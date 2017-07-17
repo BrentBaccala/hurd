fdbd35238d1db2ac2df519243d9b41d4203c7df2
Brent Baccala <cosine@freesoft.org>
2017-07-17 01:41:11 -0400

    libpager: Improved test program
    libpager-test now includes a built-in libpager that serves up
    memory-backed data, and can run tests against either its built-in
    libpager or the libpager backing a filename.  libpager-test is also
    now a 'make' target, though it is not built by default.
    
    * libpager/libpager-test.c: Add built-in libpager to serve memory
    * libpager/Makefile: add libpager-test as a make target

--- libpager/libpager-test.c
+++ libpager/libpager-test.c
@@ -10,6 +10,10 @@
 
    gcc -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -o libpager-test libpager-test.c
 
+   Basic usage (to test the built-in libpager):
+
+   libpager-test
+
    Basic usage (to test the libpager that underlies a file):
 
    libpager-test FILENAME
@@ -31,6 +35,8 @@
 #include <hurd/hurd_types.h>
 
 #include <mach/memory_object_user.h>
+
+#include "./pager.h"
 
 /***** DEBUGGING *****/
 
@@ -109,8 +115,9 @@ static const struct argp_option options[] =
     { 0 }
   };
 
-static const char args_doc[] = "PATHNAME";
-static const char doc[] = "libpager test program.";
+static const char args_doc[] = "[PATHNAME]";
+static const char doc[] = "libpager test program.  If PATHNAME is specified, run tests on the libpager backing it;\
+otherwise, use a built-in libpager for the tests.";
 
 static error_t
 parse_opt (int key, char *arg, struct argp_state *state)
@@ -132,14 +139,6 @@ parse_opt (int key, char *arg, struct argp_state *state)
           return ARGP_ERR_UNKNOWN;
         }
       break;
-
-    case ARGP_KEY_END:
-      if (state->arg_num != 1)
-        {
-          argp_usage (state);
-          return ARGP_ERR_UNKNOWN;
-        }
-      break;
     }
 
   return ESUCCESS;
@@ -152,30 +151,154 @@ const struct argp_child children[] =
 
 static struct argp argp = { options, parse_opt, args_doc, doc, children };
 
+/* PAGER CALLBACK ROUTINES
+ *
+ * The test program is linked with libpager to implement a simple
+ * memory-backed object that can be used as a testing target.
+ */
+
+#define BUFFER_SIZE 4096
+
+char buffer[BUFFER_SIZE];
+
+error_t pager_read_page (struct user_pager_info *PAGER,
+          vm_offset_t PAGE, vm_address_t *BUF, int *WRITE_LOCK)
+{
+  /*
+     For pager PAGER, read one page from offset PAGE.  Set '*BUF' to be
+     the address of the page, and set '*WRITE_LOCK' if the page must be
+     provided read-only.  The only permissible error returns are 'EIO',
+     'EDQUOT', and 'ENOSPC'.
+  */
+  /* return EIO; */
+  char * buf = malloc(__vm_page_size);
+  memcpy(buf, buffer + PAGE, __vm_page_size);
+  *BUF = (vm_address_t) buf;
+  *WRITE_LOCK = TRUE;
+  return ESUCCESS;
+}
+
+error_t pager_write_page (struct user_pager_info *PAGER,
+          vm_offset_t PAGE, vm_address_t BUF)
+{
+  /*
+     For pager PAGER, synchronously write one page from BUF to offset
+     PAGE.  In addition, 'vm_deallocate' (or equivalent) BUF.  The only
+     permissible error returns are 'EIO', 'EDQUOT', and 'ENOSPC'.
+  */
+  return EIO;
+}
+
+error_t pager_unlock_page (struct user_pager_info *PAGER,
+          vm_offset_t ADDRESS)
+{
+     /* A page should be made writable. */
+  return EIO;
+}
+
+error_t pager_report_extent
+          (struct user_pager_info *PAGER, vm_address_t *OFFSET,
+          vm_size_t *SIZE)
+{
+  /*
+     This function should report in '*OFFSET' and '*SIZE' the minimum
+     valid address the pager will accept and the size of the object.
+  */
+  *OFFSET = 0;
+  *SIZE = __vm_page_size;
+
+  return ESUCCESS;
+}
+
+void pager_clear_user_data (struct user_pager_info *PAGER)
+{
+/*
+     This is called when a pager is being deallocated after all extant
+     send rights have been destroyed.
+*/
+}
+
+void pager_dropweak (struct user_pager_info *P)
+{
+  /*
+     This will be called when the ports library wants to drop weak
+     references.  The pager library creates no weak references itself,
+     so if the user doesn't either, then it is all right for this
+     function to do nothing.
+  */
+}
+
+void
+pager_notify_evict (struct user_pager_info *pager,
+		    vm_offset_t page)
+{
+  /* Undocumented in info file */
+}
+
+
+
+
 
 /* MAIN ROUTINE */
 
 int
 main (int argc, char **argv)
 {
+  mach_port_t memobj;
+
   /* Parse our options...  */
   argp_parse (&argp, argc, argv, 0, 0, 0);
 
-  mach_port_t node = file_name_lookup (targetPath, O_RDWR, 0);
+  printf("__vm_page_size: %u\n", __vm_page_size);
 
-  if (node == MACH_PORT_NULL)
-    {
-      error (2, errno, "file_name_lookup: %s", targetPath);
+  if (targetPath != NULL) {
+
+    /* targetPath was specified; use its backing libpager for our tests */
+
+    mach_port_t node = file_name_lookup (targetPath, O_RDWR, 0);
+
+    if (node == MACH_PORT_NULL)
+      {
+        error (2, errno, "file_name_lookup: %s", targetPath);
+      }
+
+    mach_port_t memobjrd;
+    mach_port_t memobjwt;
+
+    mach_call (io_map (node, &memobjrd, &memobjwt));
+
+    printf("%lu %lu\n", memobjrd, memobjwt);
+
+    memobj = memobjwt;
+
+  } else {
+
+    /* targetPath was not specified; use our built-in libpager that serves up a memory block */
+
+    struct port_bucket * bucket = ports_create_bucket();
+
+    struct pager_requests *pager_requests;
+
+    int err = pager_start_workers (bucket, &pager_requests);
+
+    if (err) {
+      fprintf(stderr, "can't create libpager worker threads: %s", strerror (err));
+      exit(1);
     }
 
-  mach_port_t memobjrd;
-  mach_port_t memobjwt;
+    boolean_t MAY_CACHE = TRUE;
+    boolean_t NOTIFY_ON_EVICT = TRUE;
 
-  mach_call (io_map (node, &memobjrd, &memobjwt));
+    struct pager * pager = pager_create(NULL, bucket, MAY_CACHE, MEMORY_OBJECT_COPY_DELAY, NOTIFY_ON_EVICT);
 
-  printf("%d %d\n", memobjrd, memobjwt);
+    memobj = pager_get_port(pager);
 
-  mach_port_t memobj = memobjwt;
+    /* pager_get_port() gave us a receive right; we need to create a send right */
+
+    mach_call (mach_port_insert_right (mach_task_self (), memobj, memobj,
+                                       MACH_MSG_TYPE_MAKE_SEND));
+
+  }
 
   /* Create two receive rights */
 
