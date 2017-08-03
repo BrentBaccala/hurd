21e8af51b457ff0a225acab8d44fa1cc2965a596
Brent Baccala <cosine@freesoft.org>
2017-08-03 19:07:12 -0400

    libpager: new test framework can retreive a single page
    libpager's new testing framework works well enough to startup,
    request and accept a single page.
    
    * libpager/libpager-test.cc: new testing framework
    * libpager/NOTES: slight update

--- libpager/libpager-test.cc
+++ libpager/libpager-test.cc
@@ -17,12 +17,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <error.h>
 #include <argp.h>
 #include <assert.h>
+
+#include <vector>
 
 #include "machMessage.h"
 
@@ -73,6 +76,207 @@ void ddprintf(Args... rest)
 const char * targetPath = NULL;
 
 const int timeout = 1000;   /* 1 second timeout on almost all our operations */
+
+mach_port_t memobj;         /* this is the libpager memory object we'll use for all of our tests */
+                            /* this is how the pager is seen from the client */
+                            /* it needs to be initialized before calling any of these test routines! */
+
+struct pager * pager;       /* this is the libpager pager object */
+                            /* this is how the pager is seen from the translator */
+                            /* it also needs to be initialized early! */
+                            /* if we operate in the mode where we're talking to a disk file, this never gets used */
+
+/* translator operations such as pager_read_page() suspend until translator_complete_operation()
+ * is called by the testing framework.  To avoid race conditions and deadlock in the test
+ * program, we increment a counter ('operations') whenever translator_complete_operation()
+ * is called, and allow translator_suspend_operation() to proceed if the counter is positive.
+ * We only suspend when the number of suspend calls exceeds the number of complete calls
+ * (so far).
+ */
+
+pthread_mutex_t mutex = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int operations = 0;
+
+void translator_complete_operation(void)
+{
+  pthread_mutex_lock (&mutex);
+  operations ++;
+  pthread_cond_signal (&cond);
+  pthread_mutex_unlock (&mutex);
+}
+
+void translator_suspend_operation(void)
+{
+  pthread_mutex_lock (&mutex);
+  if (operations == 0) {
+    pthread_cond_wait (&cond, &mutex);
+  }
+  operations --;
+  pthread_mutex_unlock (&mutex);
+}
+
+class client {
+public:
+  struct page {
+    void * ptr;
+    boolean_t precious;
+    vm_prot_t access;
+  };
+  std::vector<struct page> pageptrs;
+
+  mach_port_t memory_control;
+  mach_port_t memory_object_name;
+
+  size_t page_size = __vm_page_size;
+
+  client(void)
+  {
+
+    /* Create two receive rights */
+
+    mach_call (mach_port_allocate (mach_task_self (), MACH_PORT_RIGHT_RECEIVE, &memory_control));
+    mach_call (mach_port_allocate (mach_task_self (), MACH_PORT_RIGHT_RECEIVE, &memory_object_name));
+
+    /* send memory_object_init */
+
+    mach_call (memory_object_init (memobj, memory_control, memory_object_name, __vm_page_size));
+
+    /* wait for the memory_object_ready (2094) in reply, but we'll get
+     * no reply from the old libpager if the kernel has already
+     * requested a memory object from this file, and even just a simple
+     * 'cat' on the file will trigger that
+     */
+
+    machMessage msg;
+
+    mach_call (mach_msg (msg, MACH_RCV_MSG | MACH_RCV_TIMEOUT,
+                         0, msg.max_size, memory_control,
+                         timeout, MACH_PORT_NULL));
+
+    printf("%d %d\n", msg->msgh_size, msg->msgh_id);
+
+    assert(msg->msgh_id == 2094); /* memory_object_ready */
+  }
+
+  void request_read_access(int start_page, int end_page)
+  {
+    /* ASSERT: page absent in client */
+    for (int page = start_page; page <= end_page; page ++) {
+      assert(page >= pageptrs.size() || pageptrs[page].ptr == nullptr);
+    }
+    /* sends m_o_data_request */
+    memory_object_data_request(memobj, memory_control,
+                               start_page * page_size, (end_page - start_page + 1) * page_size,
+                               VM_PROT_READ);
+  }
+
+  void request_write_access(int start_page, int end_page)
+  {
+    /* ASSERT: page absent in client */
+    for (int page = start_page; page <= end_page; page ++) {
+      assert(page >= pageptrs.size() || pageptrs[page].ptr == nullptr);
+    }
+    /* sends m_o_data_request */
+    mach_call(memory_object_data_request(memobj, memory_control,
+                                         start_page * page_size, (end_page - start_page + 1) * page_size,
+                                         VM_PROT_READ | VM_PROT_WRITE));
+  }
+
+  void request_unlock(int start_page, int end_page)
+  {
+    /* ASSERT: page present in client with read access */
+    for (int page = start_page; page <= end_page; page ++) {
+      assert(pageptrs[page].ptr == nullptr);
+    }
+    /* sends m_o_data_unlock */
+    memory_object_data_request(memobj, memory_control,
+                               start_page * page_size, (end_page - start_page + 1) * page_size,
+                               VM_PROT_READ | VM_PROT_WRITE);
+  }
+
+  void flush(int start_page, int end_page)
+  {
+    /* ASSERT: pages present in client */
+    for (int page = start_page; page <= end_page; page ++) {
+      assert(pageptrs[page].ptr != nullptr);
+    }
+    /* if pages have WRITE access, modify data and data_return */
+    /* if pages have READ access and are precious, data_return */
+    memory_object_data_request(memobj, memory_control,
+                               start_page * page_size, (end_page - start_page + 1) * page_size,
+                               VM_PROT_READ | VM_PROT_WRITE);
+  }
+
+  void service_message(void)
+  {
+    machMessage msg;
+
+    printf("client service_message\n");
+    /* read message */
+
+    if (mach_call (mach_msg (msg, MACH_RCV_MSG | MACH_RCV_TIMEOUT,
+                             0, msg.max_size, memory_control,
+                             timeout, MACH_PORT_NULL))
+        == (err_ipc | KERN_TIMEDOUT)) {
+      return;
+    }
+
+    printf("%d %d\n", msg->msgh_size, msg->msgh_id);
+
+    switch (msg->msgh_id) {
+    case 2094: /* memory_object_ready */
+      break;
+
+    case 2093: /* memory_object_data_supply */
+      printf("m_o_data_supply: offset = %d, data @ 0x%08x, length = %d, lock_value = %d; precious = %d\n",
+             msg[0][0], (void *) msg[1].data(), msg[1].data_size(), msg[2][0], msg[3][0]);
+
+      assert(msg[1].data_size() % page_size == 0);
+
+      /* data_supply - save pointers, read/write and precious (reply if requested) */
+
+      for (int page = 0; page < msg[1].data_size() / page_size; page ++) {
+        /* ASSERT: page absent in client */
+        assert(page >= pageptrs.size() || pageptrs[page].ptr == nullptr);
+        if (page >= pageptrs.size()) pageptrs.resize(page+1);
+        pageptrs[page].ptr = (char *) msg[1].data() + page * page_size;
+        pageptrs[page].access = msg[2][0];
+        pageptrs[page].precious = msg[2][0];
+      }
+      break;
+
+    case 2044: /* memory_object_lock_request */
+      /* data_lock - send messages, update pointers, reply if requested */
+
+    case 2090: /* memory_object_data_error */
+      /* data_error - ?? */
+
+    default:
+      printf("unknown message\n");
+    }
+  }
+};
+
+void test_bug1(void)
+{
+  client cl;  /* creating the client does the m_o_init / m_o_ready exchange */
+
+  cl.request_write_access(0, 0);
+  translator_complete_operation();
+  cl.service_message();
+#if 0
+  pager_sync(pager, 0);
+  pager_sync(pager, 0);
+  pager_sync(pager, 0);
+  cl.service_message();
+  cl.service_message();
+  cl.service_message();
+  translator_complete_operation();
+  translator_complete_operation();
+  translator_complete_operation();
+#endif
+}
 
 
 /***** COMMAND-LINE OPTIONS *****/
@@ -143,6 +347,9 @@ error_t pager_read_page (struct user_pager_info *PAGER,
   memcpy(buf, buffer + PAGE, __vm_page_size);
   *BUF = (vm_address_t) buf;
   *WRITE_LOCK = TRUE;
+
+  translator_suspend_operation();
+
   return ESUCCESS;
 }
 
@@ -212,8 +419,6 @@ pager_notify_evict (struct user_pager_info *pager,
 int
 main (int argc, char **argv)
 {
-  mach_port_t memobj;
-
   /* Parse our options...  */
   argp_parse (&argp, argc, argv, 0, 0, 0);
 
@@ -257,7 +462,7 @@ main (int argc, char **argv)
     boolean_t MAY_CACHE = TRUE;
     boolean_t NOTIFY_ON_EVICT = TRUE;
 
-    struct pager * pager = pager_create(NULL, bucket, MAY_CACHE, MEMORY_OBJECT_COPY_DELAY, NOTIFY_ON_EVICT);
+    pager = pager_create(NULL, bucket, MAY_CACHE, MEMORY_OBJECT_COPY_DELAY, NOTIFY_ON_EVICT);
 
     memobj = pager_get_port(pager);
 
@@ -266,8 +471,10 @@ main (int argc, char **argv)
     mach_call (mach_port_insert_right (mach_task_self (), memobj, memobj,
                                        MACH_MSG_TYPE_MAKE_SEND));
 
+    test_bug1();
   }
 
+#if 0
   /* Create two receive rights */
 
   mach_port_t memory_control;
@@ -349,5 +556,7 @@ main (int argc, char **argv)
     printf("%d %d\n", msg->msgh_size, msg->msgh_id);
 
   }
+
+#endif
 
 }
